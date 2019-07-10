dda4dca9e6f9cca28d15324b436ec0017f4198ad
Yuichi Motoyama <yomichi@tsg.jp>
2019-07-10 21:58:17 +0900

    update

---
+++ .travis_scripts/deploy_docs.sh
@@ -0,0 +1,32 @@
+## rewrite TODO sections for your project
+
+# This is a pull request, finish.
+if [ "_$TRAVIS_PULL_REQUEST" != "_false" ] ;then exit 0; fi
+# This is neither master nor tag, finish.
+if [ "_$TRAVIS_BRANCH" != "_master" ] && [ ${TRAVIS_BRANCH%-autodoc} != ${TRAVIS_BRANCH} ] && [ -z "$TRAVIS_TAG" ] ; then exit 0; fi
+
+feature_branch=${TRAVIS_BRANCH%-autodoc}
+
+openssl aes-256-cbc -K $encrypted_aa0e0f6aad31_key -iv $encrypted_aa0e0f6aad31_iv -in ${ROOTDIR}/.travis_scripts/ssh_key.enc -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
+git clone git@github.com:${TRAVIS_REPO_SLUG} dcore-repo
+cd dcore-repo
+git checkout gh-pages
+if [ ${feature_branch} == ${TRAVIS_BRANCH} ]; then
+  cp -r ${ROOTDIR}/dcore_doc $feature_branch
+  git add $feature_branch
+else
+  cp -r ${ROOTDIR}/dcore_doc/* .
+  git add .
+fi
+
+git config user.email ""
+git config user.name "DCore"
+git commit -m "Update by TravisCI (\\#${TRAVIS_BUILD_NUMBER})"
+ST=$?
+if [ $ST == 0 ]; then
+  git push origin gh-pages:gh-pages --follow-tags > /dev/null 2>&1
+fi
+
